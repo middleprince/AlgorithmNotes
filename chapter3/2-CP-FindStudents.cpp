4adb5549c98dfdd45dcba342f62ff34f6d057aeb
middleprince <hellangel.ah@gmail.com>
2020-12-25 11:58:41 +0800

    trival

--- chapter3/2-CP-FindStudents.cpp
+++ chapter3/2-CP-FindStudents.cpp
@@ -15,37 +15,32 @@ struct student{
 int main()
 {
     int N, M;
-    char who[6];
-    int temp;
+    int who;
     student *buffer;
     
+    scanf("%d", &N); 
     //FIXED : multi inputs 
-    while(~scanf("%d", &N)){
-        student *students_info[1010] = {nullptr};
-        //for(int i=0; i<200; ++i)
-        //    if(students_info[i] == nullptr) printf("INFO nullptr\n");
+    while(N--){
+        student *students_info[24] = {nullptr};
+        scanf("%d", &M);
 
-        while(N--){ 
+        while(M--){ 
             buffer = (student*) malloc(sizeof(student));
             scanf("%d %s %s %d", &(buffer->id), buffer->name, buffer->sex, &(buffer->year_old)); 
             students_info[buffer->id] = buffer; 
         }
         
-        scanf("%d", &M);
-        while(M--){
-            // query format is too tricky
-            //memset(who, ' ', sizeof(who));
-            scanf("%s", who);
-            sscanf(who, "%d", &temp); 
-            
-            if(students_info[temp] == nullptr){
-                printf("No Answer!\n");
-                continue;
-            }
-            buffer = students_info[temp];
-            printf("%s %s %s %d\n", who, buffer->name, buffer->sex, buffer->year_old);
-        }
+       // query format is too tricky
+       //memset(who, ' ', sizeof(who));
+       scanf("%d", &who);
+       if(students_info[who] == nullptr){
+           printf("No Answer!\n");
+           continue;
+       }
+       buffer = students_info[who];
+       printf("%d %s %s %d\n", who, buffer->name, buffer->sex, buffer->year_old);
     }
+    
     
     return 0;
 }
