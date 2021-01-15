1e86a75acf0ea2ca3275906a1ac05efafd4f6161
middleprince <hellangel.ah@gmail.com>
2021-01-15 18:25:34 +0800

    fix: Fix the output format bug

--- chapter4/1-CP-CexcelSort.cpp
+++ chapter4/1-CP-CexcelSort.cpp
@@ -1,30 +1,36 @@
 #include<cstdio>
 #include<algorithm>
 #include<vector>
-#include<string>
+#include<cstring>
 
 using std::vector;
-using std::string;
+//using std::string;
 
 struct Student{
     int no;
-    string name;
+    //string name;
+    char name[20];
     int grades;
 };
     
-// TODO: Fix error 50
+// FIXED: Fix error 50
 int main()
 {
-   int n, c; 
-   vector<Student> data;
-   while(scanf("%d %d", &n, &c) != EOF)
-   {
-        int temp_no, temp_grade; 
-        char name[20];
+
+    int n, c, item = 0; 
+    vector<Student> data;
+    while(scanf("%d %d", &n, &c) != EOF && n!=0)
+    {
+        ++item;
+        data.clear();
+        //int temp_no, temp_grade; 
+        //char name[20];
+        Student in;
+
         while(n--)
         {
-            scanf("%d %s %d", &temp_no, name, &temp_grade);
-            data.push_back(Student{temp_no, name, temp_grade}); 
+            scanf("%d %s %d", &(in.no), in.name, &(in.grades));
+            data.push_back(in); 
         }
         
         switch(c)
@@ -37,22 +43,25 @@ int main()
             case 2:
                 std::sort(data.begin(), data.end(), 
                         [] (const Student& a, const Student &b) 
-                           {return a.name.compare(b.name);} ); 
+                           //{return a.name.compare(b.name) == 0 ? (a.no < b.no) : (a.name.compare(b.name) <= 0);} ); 
+                           {return strcmp(a.name, b.name) == 0 ? (a.no < b.no) : (strcmp(a.name, b.name) < 0);} ); 
                 break;
                         
             case 3:
                 std::sort(data.begin(), data.end(), 
                         [] (const Student& a, const Student &b) 
-                           {return a.grades <= b.grades;} ); 
+                           {return a.grades == b.grades ? (a.no < b.no) : (a.grades < b.grades);} ); 
                 break;
         }
 
+        // fixed the bug, which i print state c instead of ith case. 
+        printf("Case %d:\n", item);
         for(auto const& iter : data)
         {
-            printf("%06d %s %d\n", iter.no, iter.name.c_str(), iter.grades); 
+            printf("%06d %s %d\n", iter.no, iter.name, iter.grades); 
         }
         
-   }
+    }
     
 }
 
